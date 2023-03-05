233261caefb6e71c89e20950d6bd493decda5680
NiksBors <127048873+NiksBors@users.noreply.github.com>
2023-03-05 20:49:53 +0000

    -

--- tree_height.py
+++ tree_height.py
@@ -31,17 +31,15 @@ def main():
         fileName = str(input())
         if "a" in fileName:
             print("fails satur a")
-            try:
-                file="test/" + str(fileName)
-                with open(file,'r') as jaunsf:
-                    n = int(jaunsf.readline())
-                    parent =cc.array(list(map(int, jaunsf.readline().split())))
-            except FileNotFoundError:
-                print("fails nav atrasts")
-                return
-        else:
-            print("error")
+        try:
+            file="test/" + str(fileName)
+            with open(file,'r') as jaunsf:
+                 n = int(jaunsf.readline())
+                 parent =cc.array(list(map(int, jaunsf.readline().split())))
+        except FileNotFoundError:
+            print("fails nav atrasts")
             return
+    
     
     atrisinajums = compute_height(n,parent)
     print(atrisinajums)

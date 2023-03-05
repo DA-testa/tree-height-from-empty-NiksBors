d3526f0ca52dfa6cded539d251fdc831c34c8b36
NiksBors <127048873+NiksBors@users.noreply.github.com>
2023-03-05 19:25:38 +0000

    -

--- tree_height.py
+++ tree_height.py
@@ -32,7 +32,7 @@ def main():
     text = input("Ievadat:")
     if "F" in text:
         fileName = input()
-        file='./test/'+fileName
+        file='/test/'+fileName
         if "a" not in fileName:
             try:
                 with open(file) as jaunsf:

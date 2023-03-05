fd7a80b93ac43d6530f36ebff151679760864fe4
NiksBors <127048873+NiksBors@users.noreply.github.com>
2023-03-05 21:37:43 +0000

    -

--- tree_height.py
+++ tree_height.py
@@ -5,19 +5,19 @@ import sys
 import threading
 import numpy as cc  
 def compute_height(n, parent):
-    limeni = {}
-    maxgar = 0
-    for j in range(n):
+    limeni = {} # izveido jaunu vārdnīcu/ objektu
+    maxgar = 0 # pievienoju visgarāko mezglu, un pielīdzinu nullei
+    for j in range(n): # izveido ciklu, lai izietu cauri katram koka mezglam, un seko cauri katram punktam līdz tiek pie vecāka, jeb -1
         garums = 0
         z=j
-        while z!=-1:
+        while z!=-1: # kamēr nav sasniegts vecāks, liek koka garumam klāt 1nieku.
             if z in limeni:
                 garums += limeni[z]
                 break
             garums +=1
              
             z=parent[z]
-        z = j
+        z = j # tagad pāŗbauda vai virs mezgliem nav vēl koka daļas, ja ir jau sasniegts -1, tad iziet no cikla, taču, ja saprot, ka ir aprēķinātst tikai subtree, tad palielina garumu
         while z != -1:
             if z in limeni:
                 garums += limeni[z]
@@ -30,29 +30,29 @@ def compute_height(n, parent):
 
         
 
-def main():
+def main(): # funkcija apstrādās I un F ievadi, sākumā pārbaudot, vai lietotājs ir ievadījis I, ja ir ievadījis, tad sāk nolasīt mezglu un vecāku msaīvu skaitu
    
     text = input("Ievadat: ")
     if "I" in text:
-        n = int(input().strip())
+        n = int(input())
 
         parent = cc.array(list(map(int,input().split())))
-    elif "F" in text:
+    elif "F" in text: # ja lietotājs ir ievadījis F, tad nolasa mezglu daudzumu un vecāku masīvu no faila, pēcāk pāŗbauda vai faila nosaukumā nav a burts
         fileName = str(input())
         if "a" in fileName:
             print("fails satur a")
         try:
             fileName="test/" + str(fileName)
-            with open(fileName,'r', encoding="utf8") as jaunsf:
-                 n = int(jaunsf.readline().strip())
+            with open(fileName,'r') as jaunsf:
+                 n = int(jaunsf.readline())
                  parent =cc.array(list(map(int, jaunsf.readline().split())))
         except FileNotFoundError:
             print("fails nav atrasts")
             return
     
     
-    atrisinajums = compute_height(n,parent)
-    print(atrisinajums)
+     # šeit izprintē
+    print( compute_height(n,parent))
 
     
     

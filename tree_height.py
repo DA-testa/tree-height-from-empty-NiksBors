ceef1edfbd5443359a8f75577ddce6e76a2424e0
NiksBors <127048873+NiksBors@users.noreply.github.com>
2023-03-05 18:49:50 +0000

    -

--- tree_height.py
+++ tree_height.py
@@ -1,59 +1,47 @@
-# python3
-
-import readline
 import sys
 import threading
-import numpy  
-def compute_height(n, parent):
-    adj_list = [[]for _ in range(n)]
-    for j, v in enumerate(parents):
+import numpy as np
 
-        if  v!=-1:
-            adj_list[v].append(i)
-    
-    stack =[(parents.index(-1),1)]
-    max_height =0
-    while stack:
-        l, garums = stack.pop()
-        max_height = max(max_height, garums)
-        for q in adj_list[v]:
-            stack.append((q height +1))
-    
-
-    return max_height
-
-
-
-   
-  t
 
 def main():
-    text = input("Ievadat:")
-    if "F" in text:
-        fileName = input()
+  letter = input()
+  if "I" in letter:
+    count = int(input())
+    main = list(map(int,input().split()))
+    print(compute_height(count, main))
+    
+  elif "F" in letter:
+    filename = input()
+    if "a" not in filename:
+      with open(str("test/"+filename), mode="r") as fails:
+        count = int(fails.readline())
+        main = list(map(int, fails.readline().split()))
+        print(compute_height(count, main))
         
-        if ".a" in fileName:
-            return
-        if ".a" not in fileName:
-            jaunsf = "test/"+fileName
-            with open(jaunsf, 'o') as jap:
-                n = int(jap,readline())
-            tree = compute_height(n,parent)
-            print(tree)
-    elif "I" in text:
-        n = input()
-        parent = list(map(input().split()))
 
-    
-    
-    
-    
-    
+def compute_height(n, parents):
+    zeros = np.zeros(n)
+    def height(i):
+        if zeros[i] != 0:
+            return zeros[i]
+        elif parents[i] == -1: 
+            zeros[i] = 1
+        else: 
+            zeros[i] = height(parents[i]) + 1
+        return zeros[i]
+
+    for i in range(n):
+        height(i)
+    return max(zeros)
+
+#     # input number of elements
+#     # input values in one variable, separate with space, split these values in an array
+#     # call the function and output it's result
+
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
 sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
+threading.stack_size(2**27)  # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
 # print(numpy.array([1,2,3]))
